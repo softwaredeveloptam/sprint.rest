1a5b3c04d855dd583e1e98ed25c7ef193da136ed
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-04-20 20:27:12 +0900

    second commit
    Co-authored-by: softwaredeveloptam <softwaredeveloptam@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -11,31 +11,61 @@ const setupServer = () => {
   app.use(express.json());
 
   app.get("/api/pokemon", (request, response) => {
+    const indexOfPokemon =
+      undefined || findPokemonIndexByName(request.query.name);
+    const id = undefined; // || removeZeros(request.query.id);
     const limit = undefined || request.query.limit;
-    if (limit) {
-      response.send(pokeData.pokemon.slice(0, limit));
-    } else response.send(pokeData.pokemon);
+
+    function removeZeros(strNum) {
+      let num;
+      if (strNum.charAt(0) === "0") {
+        num = strNum
+          .split("")
+          .slice(1, strNum.length)
+          .join("");
+      } else {
+        num = Number(strNum);
+      }
+      return num;
+    }
+
+    function findPokemonIndexByName(name) {
+      for (let i = 0; i < pokeData.pokemon.length; i++) {
+        if (pokeData.pokemon[i].name === name) {
+          return i;
+        }
+      }
+    }
+
+    // if (limit) {
+    //   response.send(pokeData.pokemon.slice(0, limit));
+    // } else response.send(pokeData.pokemon);
+    // id = 010, 10: Number(id) =??
+    if (id) {
+      response.send(pokeData.pokemon[Number(id) - 1]);
+    } else if (indexOfPokemon >= 0) {
+      response.send(pokeData.pokemon[indexOfPokemon]);
+    }
   });
 
   app.post("/api/pokemon", (request, response) => {
     response.send(request.body);
   });
 
-  // app.patch("/api/pokemon", (request, response) => {
-  //   const id = undefined || request.query.id;
-  //   const name = undefined || request.query.name;
+  app.patch("/api/pokemon", (request, response) => {
+    const id = undefined || request.query.id;
+    // const name = undefined || request.query.name;
 
-  //   if (id) {
-  //     for(let i = 0; i < pokeData.pokemon.length; i++) {
-  //       if (id === pokeData.pokemon[i].id) {
-  //         pokeData.pokemon[i] = request.body;
-  //         response.send(pokeData.pokemon[i]);
-  //         break;
-  //       }
-  //     }
-  //   }
-
-  // });
+    if (id) {
+      for (let i = 0; i < pokeData.pokemon.length; i++) {
+        if (id === pokeData.pokemon[i].id) {
+          pokeData.pokemon[i] = request.body;
+          response.send(pokeData.pokemon[i]);
+          break;
+        }
+      }
+    }
+  });
 
   // app.get("/api/pokemon/:id")
 
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -19,8 +19,8 @@ describe("Pokemon API Server", () => {
     request = chai.request(server);
   });
 
-  xdescribe("GET", () => {
-    describe("/api/pokemon", () => {
+  describe("GET", () => {
+    xdescribe("/api/pokemon", () => {
       it("should return all pokemon", async () => {
         const response = await request.get("/api/pokemon");
         expect(JSON.parse(response.text).length == pokeData.pokemon.length).to
@@ -34,55 +34,83 @@ describe("Pokemon API Server", () => {
 
         expect(JSON.parse(response.text).length == 100).to.be.true;
       });
-    });
-  });
 
-  /*
+      /*
+
+      GET /api/pokemon/:id
+      It should return the Pokemon with the given id. 
+      Example: GET /api/pokemon/042 should return the data for Golbat
+      Leading zeroes should not be necessary, so GET /api/pokemon/42 would also return Golbat
+
+      */
+    });
+
+    xdescribe("/api/pokemon/:id", () => {
+      it("should return pokemon with the given ID", async () => {
+        const response = await request.get("/api/pokemon").query({ id: 1 });
+
+        expect(JSON.parse(response.text)).to.be.deep.equal(pokeData.pokemon[0]);
+      });
+    });
+
+    describe("/api/pokemon/:name", () => {
+      it("should return pokemon with the given name", async () => {
+        const response = await request
+          .get("/api/pokemon")
+          .query({ name: "Bulbasaur" });
+
+        expect(JSON.parse(response.text)).to.be.deep.equal(pokeData.pokemon[0]);
+      });
+    });
+    // });
+
+    /*
     POST /api/pokemon
       It should add a Pokemon.
   */
 
-  xdescribe("POST", () => {
-    describe("/api/pokemon", () => {
-      it("should add Pokemon", async () => {
-        const expected = {
-          name: "Tam",
-          honey: ["I", "shrank", "the", "kids"],
-          loopy: {
-            loop: {
-              deeply: {
-                nested: [1, "123", [{ lol: "lol" }, null, null, 5]],
+    xdescribe("POST", () => {
+      describe("/api/pokemon", () => {
+        it("should add Pokemon", async () => {
+          const expected = {
+            name: "Tam",
+            honey: ["I", "shrank", "the", "kids"],
+            loopy: {
+              loop: {
+                deeply: {
+                  nested: [1, "123", [{ lol: "lol" }, null, null, 5]],
+                },
               },
             },
-          },
-        };
-        const response = await request.post("/api/pokemon").send(expected);
-        expect(JSON.parse(response.text)).to.be.deep.equal(expected);
+          };
+          const response = await request.post("/api/pokemon").send(expected);
+          expect(JSON.parse(response.text)).to.be.deep.equal(expected);
+        });
       });
     });
-  });
-  /*
+
+    /*
 
   PATCH /api/pokemon/:idOrName
     It should allow you to make partial modifications to a Pokemon
 
   */
 
-  // describe("PATCH", () => {
-  //   describe("/api/pokemon/:idOrName", () => {
-  //     it("should allow you to make partial modifications to a Pokemon", () => {
-  //       const response = await request.patch("/api/pokemon").query({id:1});
+    // describe("PATCH", () => {
+    //   describe("/api/pokemon/:idOrName", () => {
+    //     it("should allow you to make partial modifications to a Pokemon", () => {
+    //       const response = await request.patch("/api/pokemon").query({id:1});
 
-  //     });
-  //   });
+    //       expect(JSON.parse(response.text).length == 100).to.be.true;
+    //     });
+    //   });
 
-  //   describe("/api/attacks/:name", () => {
-  //     it("should modify specified attack", () => {
+    //   describe("/api/attacks/:name", () => {
+    //     it("should modify specified attack", () => {
 
-  //     });
-  //   });
-
-  // });
+    //     });
+    // });
+  });
 });
 
 //  it("POST /echo returns body content", async () => {

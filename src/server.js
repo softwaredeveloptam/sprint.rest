3db9bbf3cdbf6bc13d5fee10c24db3e4b0c2d82a
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-04-20 17:57:40 +0900

    first commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,45 @@
+const express = require("express");
 const pokeData = require("./data");
 
 const setupServer = () => {
   /**
    * Create, set up and return your express server, split things into separate files if it becomes too long!
    */
+
+  const app = express();
+
+  app.use(express.json());
+
+  app.get("/api/pokemon", (request, response) => {
+    const limit = undefined || request.query.limit;
+    if (limit) {
+      response.send(pokeData.pokemon.slice(0, limit));
+    } else response.send(pokeData.pokemon);
+  });
+
+  app.post("/api/pokemon", (request, response) => {
+    response.send(request.body);
+  });
+
+  // app.patch("/api/pokemon", (request, response) => {
+  //   const id = undefined || request.query.id;
+  //   const name = undefined || request.query.name;
+
+  //   if (id) {
+  //     for(let i = 0; i < pokeData.pokemon.length; i++) {
+  //       if (id === pokeData.pokemon[i].id) {
+  //         pokeData.pokemon[i] = request.body;
+  //         response.send(pokeData.pokemon[i]);
+  //         break;
+  //       }
+  //     }
+  //   }
+
+  // });
+
+  // app.get("/api/pokemon/:id")
+
+  return app;
 };
 
 module.exports = { setupServer };
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -2,16 +2,102 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
+const pokeData = require("../src/data");
+const { expect } = require("chai");
 
 /*
  * This sprint you will have to create all tests yourself, TDD style.
  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
  * The same kind of structure that you encountered in lecture.express will be provided here.
  */
+
 const server = setupServer();
+
 describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
     request = chai.request(server);
   });
+
+  xdescribe("GET", () => {
+    describe("/api/pokemon", () => {
+      it("should return all pokemon", async () => {
+        const response = await request.get("/api/pokemon");
+        expect(JSON.parse(response.text).length == pokeData.pokemon.length).to
+          .be.true;
+      });
+
+      it("should take query parameter `limit=n` that makes endpoint return first `n` Pokemon", async () => {
+        const response = await request
+          .get("/api/pokemon")
+          .query({ limit: 100 });
+
+        expect(JSON.parse(response.text).length == 100).to.be.true;
+      });
+    });
+  });
+
+  /*
+    POST /api/pokemon
+      It should add a Pokemon.
+  */
+
+  xdescribe("POST", () => {
+    describe("/api/pokemon", () => {
+      it("should add Pokemon", async () => {
+        const expected = {
+          name: "Tam",
+          honey: ["I", "shrank", "the", "kids"],
+          loopy: {
+            loop: {
+              deeply: {
+                nested: [1, "123", [{ lol: "lol" }, null, null, 5]],
+              },
+            },
+          },
+        };
+        const response = await request.post("/api/pokemon").send(expected);
+        expect(JSON.parse(response.text)).to.be.deep.equal(expected);
+      });
+    });
+  });
+  /*
+
+  PATCH /api/pokemon/:idOrName
+    It should allow you to make partial modifications to a Pokemon
+
+  */
+
+  // describe("PATCH", () => {
+  //   describe("/api/pokemon/:idOrName", () => {
+  //     it("should allow you to make partial modifications to a Pokemon", () => {
+  //       const response = await request.patch("/api/pokemon").query({id:1});
+
+  //     });
+  //   });
+
+  //   describe("/api/attacks/:name", () => {
+  //     it("should modify specified attack", () => {
+
+  //     });
+  //   });
+
+  // });
 });
+
+//  it("POST /echo returns body content", async () => {
+//         const expected = {
+//           foo: "bar",
+//           honey: ["I", "shrank", "the", "kids"],
+//           loopy: {
+//             loop: {
+//               deeply: {
+//                 nested: [1, "123", [{ lol: "lol" }, null, null, 5]],
+//               },
+//             },
+//           },
+//         };
+//         const res = await request.post("/echo").send(expected);
+//         res.should.be.json;
+//         JSON.parse(res.text).should.deep.equal(expected);
+//       });
